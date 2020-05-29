5185b1e09ff442c19dc9bef638c90d0bf2122ddb
RobiFag <robifagandini@hotmail.it>
2020-05-29 18:34:07 +0200

    new scripts

--- fill_columns.py
+++ fill_columns.py
@@ -33,12 +33,46 @@ lyr = iface.activeLayer()
 #    lyr.changeAttributeValue(f_id, pfield_id, values)
 
 #cambio valore COLONNA N_DTM O N_DSM per una campagna
-for f in lyr.getSelectedFeatures():
+#for f in lyr.getSelectedFeatures():
+#    f_id = f.id()
+#    pfield_id = f.fields().indexFromName('N_DSM')
+#    str_values = str(f["N_DSM"]).split("__")
+#    if len(str_values) > 1:
+#        #print(str_values)
+#        values = (str_values[0] + '_' + str_values[1])
+#        #print(values)
+#        lyr.changeAttributeValue(f_id, pfield_id, values)
+
+
+#RIEMPO COLONNE path_1 e path_2 (CS NOVARA)
+#for f in lyr.getFeatures():
+#    f_id = f.id()
+#    p1field_id = f.fields().indexFromName('path_1')
+#    p2field_id = f.fields().indexFromName('path_2')
+#    str_values = str(f["percorso"]).split("\SP")
+##    print(str_values[0])
+##    print('\SP' + str_values[1])
+#    if len(str_values) > 0:
+#        values1 = str_values[0]
+#        values2 = '\SP' + str_values[1]
+#        lyr.changeAttributeValue(f_id, p1field_id, values1)
+#        lyr.changeAttributeValue(f_id, p2field_id, values2)
+#print('TERMINATO')
+
+#ESTRAGGO VALORI UNIVOCI DI PRIMA PARTE DEL PERCORSO DA COLONNA 'percorso' (CS NOVARA)
+
+path  = []
+for f in lyr.getFeatures():
     f_id = f.id()
-    pfield_id = f.fields().indexFromName('N_DSM')
-    str_values = str(f["N_DSM"]).split("__")
-    if len(str_values) > 1:
-        #print(str_values)
-        values = (str_values[0] + '_' + str_values[1])
-        #print(values)
-        lyr.changeAttributeValue(f_id, pfield_id, values)
+#    p1field_id = f.fields().indexFromName('path_1')
+#    p2field_id = f.fields().indexFromName('path_2')
+    str_values = str(f["percorso"]).split("\SP")
+#    print(str_values[0])
+#    print('\SP' + str_values[1])
+    if len(str_values) > 0:
+        path.append(str_values[0])
+set_list = set(path)
+unique = list(set_list)
+for uv in unique:
+    print(uv)
+print('TERMINATO')
---
+++ get_cod_label_widget.py
@@ -0,0 +1,57 @@
+import os
+
+#the table layer name
+table = QgsProject.instance().mapLayersByName('valuemap')[0]
+#index of field containing code
+idxC = table.fields().indexOf('cod')
+#index of field containing label
+idxD = table.fields().indexOf('descrizion')
+#index of field containing the field name
+idxF = table.fields().indexOf('fieldname')
+pr = table.dataProvider()
+projectInstance = QgsProject.instance()
+root = projectInstance.layerTreeRoot()
+lista = []
+layernames = []
+proj_dir = projectInstance.homePath()
+#the txt file in which the name of layers with valuemap/valurelation widgets
+txt_file = os.path.join(proj_dir,'layer.txt')
+
+for child in root.findLayers():
+    #check if layer is vector with geom
+    if isinstance(child.layer(), QgsVectorLayer) and child.layer().geometryType() != 4:
+        lyr = child.layer()
+        #get the name of layer with valuemap/valurelation widgets
+        for f in lyr.fields():
+            if f.editorWidgetSetup().type() == 'ValueMap' or f.editorWidgetSetup().type() == 'ValueRelation':
+                layernames.append(lyr.name())
+        #iterate over all fields of the layer
+        for idx in lyr.fields().allAttributesList():
+            if lyr.editorWidgetSetup(idx).type() == 'ValueMap' or lyr.editorWidgetSetup(idx).type() == 'ValueRelation':
+                fieldname = lyr.fields().field(idx).name()
+                #iterate over cod/label of valuemap/valuerelation widget
+                for dicts in lyr.editorWidgetSetup(idx).config().values():
+                    for d in dicts:
+                        for k, v in d.items():
+                            lista.append([v, k, fieldname])
+
+#write values in the table excluding duplicated records
+list_set = set(map(tuple,lista))
+unique = list(map(list,list_set))
+table.startEditing()
+for u in unique:
+    feat = QgsFeature(table.fields())
+    feat.setAttribute(idxC, u[0])
+    feat.setAttribute(idxD, u[1])
+    feat.setAttribute(idxF, u[2])
+    pr.addFeature(feat)
+table.commitChanges()
+
+#write layers names in the txt file
+name_set = set(layernames)
+unique_name = list(name_set)
+with open(txt_file, "w") as file:
+    for un in unique_name:
+        file.write(un + '\n')
+file.close()
+print('FINITO')
